87fe9527bf67c836a56c350ab7d87e8924ac37e3
Vincent Jacques <vincent@vincent-jacques.net>
2018-03-21 08:22:31 +0100

    Use Cordova to generate an Android application

---
+++ src/collide_cordova.ml
@@ -0,0 +1,42 @@
+open General.Abbr
+open Collide
+
+let get_by_id id coerce =
+  Js.Opt.get (coerce (Dom_html.getElementById id)) (fun () -> Exn.(raise NotFound))
+
+module App = GraphicalApplication.Make(struct
+  module Cairo = JsOfOCairo
+
+  module GraphicalView = struct
+    let graphical_view = get_by_id "graphical_view" Dom_html.CoerceTo.canvas
+
+    let with_context f =
+      f (JsOfOCairo.create graphical_view)
+
+    let size () =
+      (200, 200)
+
+    let on_refresh_needed f =
+      (* No refresh event on HTML canvas; just draw it now *)
+      f ()
+  end
+
+  module Timer = struct
+    let set_recurring ~seconds f =
+      Dom_html.window##setInterval (Js.wrap_callback f) (seconds *. 1000.)
+      |> ignore
+  end
+
+  module Toolbar = struct
+    let on_save_clicked _ =
+      ()
+  end
+
+  module File = struct
+    let save _ =
+      ()
+
+    let on_file_loaded _ =
+      ()
+  end
+end)
